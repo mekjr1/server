5482b409a8adaa2a5981a2443130ca4802afdd80
Daniel Calviño Sánchez <danxuliu@gmail.com>
2018-07-10 12:32:12 +0200

    Add "sendPasswordByTalk" property to shares
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/private/Share20/Share.php
+++ lib/private/Share20/Share.php
@@ -61,6 +61,8 @@ class Share implements \OCP\Share\IShare {
 	private $expireDate;
 	/** @var string */
 	private $password;
+	/** @var bool */
+	private $sendPasswordByTalk = false;
 	/** @var string */
 	private $token;
 	/** @var int */
@@ -380,6 +382,21 @@ class Share implements \OCP\Share\IShare {
 	 */
 	public function getPassword() {
 		return $this->password;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function setSendPasswordByTalk(bool $sendPasswordByTalk) {
+		$this->sendPasswordByTalk = $sendPasswordByTalk;
+		return $this;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getSendPasswordByTalk(): bool {
+		return $this->sendPasswordByTalk;
 	}
 
 	/**
